3689a58544b250de99f29ad7a5d21353eda5e0fb
BartlomiejCzerwinski <bartek2002@interia.pl>
2024-09-19 20:11:28 +0200

    add login page

--- jobit/users/views.py
+++ jobit/users/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render, HttpResponse
-
+from .forms import LoginForm
 
 def index(request):
-    return render(request, "users/index.html")
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            # login...
+    else:
+        form = LoginForm()
+
+    return render(request, 'users/login.html', {'form': form})
