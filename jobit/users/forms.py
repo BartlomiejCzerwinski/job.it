220a8f25b1a14c9ea204ec7131c3260c0584a41f
BartlomiejCzerwinski <bartek2002@interia.pl>
2024-09-19 20:17:56 +0200

    add RegisterForm form

--- jobit/users/forms.py
+++ jobit/users/forms.py
@@ -1,5 +1,14 @@
 from django import forms
 
+
 class LoginForm(forms.Form):
     email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}), label='Password')
+
+
+class RegisterForm(forms.Form):
+    first_name = forms.CharField(label='First Name', min_length=2, max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(label='Last Name', min_length=2, max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}), label='Password')
+    repeated_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}), label='Repeat Password')
