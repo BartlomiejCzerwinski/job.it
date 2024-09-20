aea2eeb09f2b58f0febd5ea0d1a86693ec265bb6
BartlomiejCzerwinski <bartek2002@interia.pl>
2024-09-20 06:35:47 +0200

    add register html

--- jobit/users/views.py
+++ jobit/users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
 
 
 def index(request):
@@ -12,9 +12,22 @@ def login(request):
         if form.is_valid():
             email = form.cleaned_data.get('email')
             password = form.cleaned_data.get('password')
+            # login...
     else:
         form = LoginForm()
     return render(request, 'users/login.html', {'form': form})
 
-def register():
-    pass
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            repeated_password = form.cleaned_data.get('repeated_password')
+            # register ...
+    else:
+        form = RegisterForm()
+    return render(request, 'users/register.html', {'form': form})
