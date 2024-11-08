0bbdab9dc7e66d0aa2868c2b9eb4819e4f9c960c
BartlomiejCzerwinski <bartek2002@interia.pl>
2024-11-08 13:41:58 +0100

    update user database model

--- jobit/users/models.py
+++ jobit/users/models.py
@@ -1,21 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
 
+class AppUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-class User(models.Model):
     RECRUITER = 'recruiter'
     WORKER = 'worker'
-
     ROLE_CHOICES = [
         (RECRUITER, 'Recruiter'),
         (WORKER, 'Worker'),
     ]
-
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=100)
-    password = models.CharField(max_length=500)
     role = models.CharField(
         max_length=10,
         choices=ROLE_CHOICES,
