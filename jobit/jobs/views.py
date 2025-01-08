1bc3a988b2c992647609195ffffe0c3e6d0c48f0
BartlomiejCzerwinski <bartek2002@interia.pl>
2025-01-08 19:51:59 +0100

    add get_user_role

--- jobit/jobs/views.py
+++ jobit/jobs/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from users.models import AppUser
 
-# Create your views here.
+
+ROLE_WORKER = "worker"
+ROLE_RECRUITER = "recruiter"
 
 
 @login_required
 def index(request):
+    role = get_user_role(request.user)
     return render(request, 'jobs/index.html')
+
+
+def get_user_role(email):
+    role = AppUser.objects.filter(user=email).first().role
+    return role
