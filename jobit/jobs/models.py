b8d45fc6757ce08a856dedfbf806bd04848277dc
BartlomiejCzerwinski <bartek2002@interia.pl>
2025-03-05 19:05:19 +0100

    add job listing DB model

--- jobit/jobs/models.py
+++ jobit/jobs/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class JobListing(models.Model):
+    CURRENCY_CHOICES = [
+        ("PLN", "PLN"),
+        ("EUR", "EUR"),
+        ("USD", "USD"),
+    ]
+
+    job_title = models.CharField(max_length=255)
+    company_name = models.CharField(max_length=255)
+    about_company = models.TextField()
+    job_description = models.TextField()
+    salary_min = models.PositiveIntegerField(null=True, blank=True)
+    salary_max = models.PositiveIntegerField(null=True, blank=True)
+    salary_currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default="PLN")
+    job_location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.job_title} at {self.company_name}"
