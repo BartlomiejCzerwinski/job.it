baa8aebab7d82a94cbde55c4829f329ec0594bd6
BartlomiejCzerwinski <bartek2002@interia.pl>
2025-03-22 18:29:23 +0100

    refactor recruiter.js

--- jobit/jobs/static/js/recruiter.js
+++ jobit/jobs/static/js/recruiter.js
@@ -1,6 +1,14 @@
 let SKILLS_URL = "http://127.0.0.1:8000/get_skills";
 let SKILLS_IN_LISTING = [];
 
+document.addEventListener("DOMContentLoaded", function () {
+    jobLocationSearchLogic();
+    addSkillModalLogic();
+    modalVisibilityLogic();
+    addSkillFromModalToJobListingLogic();
+    beforeSendListingLogic();
+});
+
 function fetchSkills(endpointUrl) {
     return fetch(endpointUrl)
         .then(response => {
@@ -31,18 +39,18 @@ function resetAddSkillModal() {
     document.getElementById("skillLevelFeedback").style.display = "none";
 }
 
-function showError(fieldId) {
+function showErrorInModal(fieldId) {
     let errorMessage = document.getElementById(fieldId);
     errorMessage.style.display = 'block';
 }
 
-function hideError(fieldId) {
+function hideErrorInModal(fieldId) {
     let errorMessage = document.getElementById(fieldId);
     errorMessage.style.display = 'none';
 }
 
 
-function loadSkills(skills) {
+function loadSkillsListInModal(skills) {
     const selectElement = document.getElementById('skillNameSelect');
     for (let i = 0; i < skills.length; i++) {
         const newOption = document.createElement('option');
@@ -52,48 +60,6 @@ function loadSkills(skills) {
         selectElement.appendChild(newOption);
     }
 }
-
-function hideModal() {
-    let addSkillModalElement = document.getElementById("addSkillModal");
-    let addSkillModal = bootstrap.Modal.getInstance(addSkillModalElement);
-    addSkillModal.hide();
-}
-
-
-// Job location logic
-document.addEventListener("DOMContentLoaded", function () {
-    const locationInput = document.getElementById("id_job_location");
-    const suggestionsDiv = document.getElementById("locationSuggestions");
-    locationInput.addEventListener("input", function () {
-        const query = locationInput.value;
-        if (query.length < 3) {
-            suggestionsDiv.innerHTML = "";
-            return;
-        }
-
-        fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${query}`)
-            .then(response => response.json())
-            .then(data => {
-                suggestionsDiv.innerHTML = "";
-                data.forEach(location => {
-                    const item = document.createElement("div");
-                    item.classList.add("list-group-item", "list-group-item-action");
-                    item.textContent = location.display_name;
-                    item.addEventListener("click", function () {
-                        locationInput.value = location.display_name;
-                        suggestionsDiv.innerHTML = "";
-                    });
-                    suggestionsDiv.appendChild(item);
-                });
-            });
-    });
-
-    document.addEventListener("click", function (event) {
-        if (!suggestionsDiv.contains(event.target)) {
-            suggestionsDiv.innerHTML = "";
-        }
-    });
-});
 
 function getSkillId(skillName) {
     let selectElement = document.getElementById('skillNameSelect');
@@ -105,56 +71,62 @@ function getSkillId(skillName) {
     }
 }
 
-// Add skill logic
-document.getElementById('addSkillButton').addEventListener('click', function () {
-    const skillName = document.getElementById('searchSkillInput').value.trim();
-    const skillLevel = document.getElementById('skillLevelSelect').value;
-
-    let isValid = true;
-
-    if (!skillName) {
-        showError('skillNameFeedback')
-        isValid = false;
-    } else {
-        hideError('skillNameFeedback')
-    }
-
-    if (skillLevel != 1 && skillLevel != 2 && skillLevel != 3) {
-        showError('skillLevelFeedback')
-        isValid = false;
-    } else {
-        hideError('skillLevelFeedback')
-    }
-
-    if (isValid) {
-        let skillId = getSkillId(skillName);
-        let skill = {"id": skillId, "name": skillName, "level": skillLevel};
-        if (addSkillToSkillsList(skill)) {
-            resetAddSkillModal();
-            hideModal();
-            console.log(`Added skill: ${skillName} - Level: ${skillLevel}`);
-        } else {
-            showError('skillExistsFeedback');
-            console.log(`Cannot add skill: ${skillName} - Level: ${skillLevel}`);
-        }
-    } else {
-        hideError('skillExistsFeedback');
-        console.log('Cannot add skill')
-    }
-});
-
-function addSkillToSkillsList(skill) {
+function addSkillToJobListing(skill) {
     for (let i = 0; i < SKILLS_IN_LISTING.length; i++) {
         if (skill.id == SKILLS_IN_LISTING[i].id && skill.name == SKILLS_IN_LISTING[i].name)
-            return false
+            return false // If skill is already present in Job listing
     }
     SKILLS_IN_LISTING.push(skill);
     addSkillItem(skill);
-    return true;
+    return true; // If skill is not present in job listing, it will be successfully added
 }
 
-// Search skill logic
-document.addEventListener("DOMContentLoaded", function () {
+function generateSkillHTML(skill) {
+    let progressBarClass = "bg-danger";
+    let progressWidth = "100%";
+    let progressValue = 100;
+
+    if (skill.level == 1) {
+        progressBarClass = "bg-success";
+        progressWidth = "33%";
+        progressValue = 33;
+    } else if (skill.level == 2) {
+        progressBarClass = "bg-warning";
+        progressWidth = "66%";
+        progressValue = 66;
+    }
+
+    return `
+        <div id="skillItem-${skill.id}">
+            <div class="d-flex align-items-center">
+                <small class="w-100 text-truncate">${skill.name}</small>
+                <i class="bi bi-x-lg ms-auto" style="font-size: 1rem; color: black; cursor: pointer;"
+                   onclick="removeSkillListItem('${skill.id}')"
+                   data-bs-toggle="modal" data-bs-target="#editSkillModal"></i>
+            </div>
+            <div class="progress mb-3" style="height: 5px;">
+                <div class="progress-bar ${progressBarClass}" role="progressbar"
+                     style="width: ${progressWidth};" aria-valuenow="${progressValue}"
+                     aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+        </div>
+    `;
+}
+
+function addSkillItem(skill) {
+    let skillsList = document.getElementById("skillsList");
+    let skillHTML = generateSkillHTML(skill);
+    skillsList.innerHTML += skillHTML;
+}
+
+function removeSkillListItem(skill) {
+    let skillItem = document.getElementById("skillItem-".concat(skill.id));
+    skillItem.innerHTML = '';
+}
+
+// Main logic ****************************************************************************
+
+function addSkillModalLogic() {
     const searchInput = document.getElementById("searchSkillInput");
     const skillSelect = document.getElementById("skillNameSelect");
     let isOptionSelected = false;
@@ -204,17 +176,50 @@ document.addEventListener("DOMContentLoaded", function () {
 
         });
     });
-});
+}
 
-// Listen on modal close
-document.addEventListener("DOMContentLoaded", function () {
+function jobLocationSearchLogic() {
+    const locationInput = document.getElementById("id_job_location");
+    const suggestionsDiv = document.getElementById("locationSuggestions");
+    locationInput.addEventListener("input", function () {
+        const query = locationInput.value;
+        if (query.length < 3) {
+            suggestionsDiv.innerHTML = "";
+            return;
+        }
+
+        fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${query}`)
+            .then(response => response.json())
+            .then(data => {
+                suggestionsDiv.innerHTML = "";
+                data.forEach(location => {
+                    const item = document.createElement("div");
+                    item.classList.add("list-group-item", "list-group-item-action");
+                    item.textContent = location.display_name;
+                    item.addEventListener("click", function () {
+                        locationInput.value = location.display_name;
+                        suggestionsDiv.innerHTML = "";
+                    });
+                    suggestionsDiv.appendChild(item);
+                });
+            });
+    });
+
+    document.addEventListener("click", function (event) {
+        if (!suggestionsDiv.contains(event.target)) {
+            suggestionsDiv.innerHTML = "";
+        }
+    });
+}
+
+function modalVisibilityLogic() {
     let skillModal = document.getElementById("addSkillModal");
 
     skillModal.addEventListener("shown.bs.modal", function () {
         console.log("Modal opened.");
         fetchSkills(SKILLS_URL)
             .then(skills => {
-                loadSkills(skills);
+                loadSkillsListInModal(skills);
             })
             .catch(error => console.error("Error while loading skills:", error));
     });
@@ -223,59 +228,52 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log("Modal closed.");
         resetAddSkillModal();
     });
-});
-
-// Skills list
-function generateSkillHTML(skill) {
-    let progressBarClass = "bg-danger";
-    let progressWidth = "100%";
-    let progressValue = 100;
-
-    if (skill.level == 1) {
-        progressBarClass = "bg-success";
-        progressWidth = "33%";
-        progressValue = 33;
-    } else if (skill.level == 2) {
-        progressBarClass = "bg-warning";
-        progressWidth = "66%";
-        progressValue = 66;
-    }
-
-    return `
-        <div id="skillItem-${skill.id}">
-            <div class="d-flex align-items-center">
-                <small class="w-100 text-truncate">${skill.name}</small>
-                <i class="bi bi-x-lg ms-auto" style="font-size: 1rem; color: black; cursor: pointer;"
-                   onclick="removeSkillListItem('${skill.id}')"
-                   data-bs-toggle="modal" data-bs-target="#editSkillModal"></i>
-            </div>
-            <div class="progress mb-3" style="height: 5px;">
-                <div class="progress-bar ${progressBarClass}" role="progressbar"
-                     style="width: ${progressWidth};" aria-valuenow="${progressValue}"
-                     aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-        </div>
-    `;
 }
 
-function addSkillItem(skill) {
-    let skillsList = document.getElementById("skillsList");
-    let skillHTML = generateSkillHTML(skill);
-    skillsList.innerHTML += skillHTML;
+function addSkillFromModalToJobListingLogic() {
+    document.getElementById('addSkillButton').addEventListener('click', function () {
+        const skillName = document.getElementById('searchSkillInput').value.trim();
+        const skillLevel = document.getElementById('skillLevelSelect').value;
+
+        let isValid = true;
+
+        if (!skillName) {
+            showErrorInModal('skillNameFeedback')
+            isValid = false;
+        } else {
+            hideErrorInModal('skillNameFeedback')
+        }
+
+        if (skillLevel != 1 && skillLevel != 2 && skillLevel != 3) {
+            showErrorInModal('skillLevelFeedback')
+            isValid = false;
+        } else {
+            hideErrorInModal('skillLevelFeedback')
+        }
+
+        if (isValid) {
+            let skillId = getSkillId(skillName);
+            let skill = {"id": skillId, "name": skillName, "level": skillLevel};
+            if (addSkillToJobListing(skill)) {
+                resetAddSkillModal();
+                let addSkillModalElement = document.getElementById("addSkillModal");
+                let addSkillModal = bootstrap.Modal.getInstance(addSkillModalElement);
+                addSkillModal.hide();
+                console.log(`Added skill: ${skillName} - Level: ${skillLevel}`);
+            } else {
+                showErrorInModal('skillExistsFeedback');
+                console.log(`Cannot add skill: ${skillName} - Level: ${skillLevel}`);
+            }
+        } else {
+            hideErrorInModal('skillExistsFeedback');
+            console.log('Cannot add skill')
+        }
+    });
 }
 
-function removeSkillListItem(skill) {
-    let skillsList = document.getElementById("skillsList");
-    let skillItem = document.getElementById("skillItem-".concat(skill.id));
-    skillItem.innerHTML = '';
+function beforeSendListingLogic() {
+    document.querySelector("form").addEventListener("submit", function () {
+        let skillsDataInput = document.getElementById("skillsListForListing");
+        skillsDataInput.value = JSON.stringify(SKILLS_IN_LISTING);
+    });
 }
-
-// Send skills logic
-function updateSkillsInput() {
-    let skillsDataInput = document.getElementById("skillsListForListing");
-    skillsDataInput.value = JSON.stringify(SKILLS_IN_LISTING);
-}
-
-document.querySelector("form").addEventListener("submit", function () {
-    updateSkillsInput();
-});
