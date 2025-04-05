3e0728c3ddcaee5467cf11be1ddab522ed8e99e1
BartlomiejCzerwinski <bartek2002@interia.pl>
2025-04-05 18:24:42 +0200

    create worker-profile-common

--- jobit/jobs/static/js/worker-profile-add-skill-modal.js
+++ jobit/jobs/static/js/worker-profile-add-skill-modal.js
@@ -210,26 +210,6 @@ function resetAddSkillModal() {
     document.getElementById("skillExistsFeedback").style.display = "none";
 }
 
-function showToast(message, type) {
-    const toastElement = document.getElementById("customToast");
-    const toastMessage = document.getElementById("toastMessage");
-
-    toastMessage.innerText = message;
-
-    toastElement.classList.remove("text-bg-primary", "text-bg-success", "text-bg-danger");
-
-    if (type === "success") {
-        toastElement.classList.add("text-bg-success");
-    } else if (type === "error") {
-        toastElement.classList.add("text-bg-danger");
-    } else {
-        toastElement.classList.add("text-bg-primary");
-    }
-
-    const toast = new bootstrap.Toast(toastElement);
-    toast.show();
-}
-
 function generateSkillHTML(skill) {
     let progressBarClass = "bg-danger";
     let progressWidth = "100%";
