2a3c4d224e18d83ce33ad1e8ffd9da34c7078830
BartlomiejCzerwinski <bartek2002@interia.pl>
2025-04-19 08:23:42 +0200

    Add application DB model

--- jobit/jobs/models.py
+++ jobit/jobs/models.py
@@ -50,3 +50,7 @@ class JobListingSkill(models.Model):
         ]
 
 
+class Application(models.Model):
+    job_listing = models.ForeignKey(JobListing, on_delete=models.CASCADE)
+    candidate = models.ForeignKey(AppUser, on_delete=models.CASCADE, null=True, blank=True)
+    applied_at = models.DateTimeField(auto_now=True)
