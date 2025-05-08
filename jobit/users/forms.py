332db13af6f46fbc30396ef7c318205e0ce5d3a3
BartlomiejCzerwinski <bartek2002@interia.pl>
2025-05-08 19:45:33 +0200

    add register placeholders

--- jobit/users/forms.py
+++ jobit/users/forms.py
@@ -24,48 +24,69 @@ class RegisterForm(forms.Form):
         label='First Name',
         min_length=2,
         max_length=50,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your first name'
+        })
     )
     last_name = forms.CharField(
         label='Last Name',
         min_length=2,
         max_length=50,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your last name'
+        })
     )
     email = forms.EmailField(
         label='Email',
-        widget=forms.EmailInput(attrs={'class': 'form-control'})
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your email address'
+        })
     )
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your password'
+        }),
         label='Password'
     )
     repeated_password = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Repeat your password'
+        }),
         label='Repeat Password'
     )
     role = forms.ChoiceField(
         choices=ROLE_CHOICES,
         label='Role',
-        widget=forms.Select(attrs={'class': 'form-select'}),
+        widget=forms.Select(attrs={
+            'class': 'form-select',
+            'placeholder': 'Select your role'
+        }),
         required=True
     )
     position = forms.CharField(
         label='Position',
         max_length=255,
-        required=False,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your job position (e.g. Full Stack Developer)'
+        })
     )
     location = forms.CharField(
         label='Location',
         max_length=255,
-        required=False,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your location (e.g. Warsaw, Poland)'
+        })
     )
     mobile = forms.CharField(
         label='Mobile',
         max_length=11,
-        required=False,
         validators=[validate_phone_number],
         widget=forms.TextInput(attrs={
             'class': 'form-control',
@@ -77,16 +98,22 @@ class RegisterForm(forms.Form):
     starts_in = forms.ChoiceField(
         choices=AppUser.STARTS_IN_CHOICES,
         label='Starts in',
-        required=False,
-        widget=forms.Select(attrs={'class': 'form-select'})
+        widget=forms.Select(attrs={
+            'class': 'form-select',
+            'placeholder': 'Select when you can start'
+        })
     )
     is_remote = forms.BooleanField(
         label='Remote',
-        required=False,
-        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'})
+        widget=forms.CheckboxInput(attrs={
+            'class': 'form-check-input',
+            'title': 'Check if you are open to remote work'
+        })
     )
     is_hybrid = forms.BooleanField(
         label='Hybrid',
-        required=False,
-        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'})
+        widget=forms.CheckboxInput(attrs={
+            'class': 'form-check-input',
+            'title': 'Check if you are open to hybrid work'
+        })
     )
