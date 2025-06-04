880775f57e7c5169621d1e18b0c7c055c3b17e64
BartlomiejCzerwinski <bartek2002@interia.pl>
2025-06-04 19:46:13 +0200

    Create locations.py

---
+++ jobit/users/locations.py
@@ -0,0 +1,38 @@
+LOCATIONS = {
+    'Poland': [
+        'Warsaw',
+        'Krakow',
+        'Wroclaw',
+        'Poznan',
+        'Gdansk',
+        'Lodz',
+        'Katowice',
+        'Lublin',
+        'Bialystok',
+        'Szczecin'
+    ],
+    'Germany': [
+        'Berlin',
+        'Munich',
+        'Hamburg',
+        'Frankfurt',
+        'Cologne',
+        'Stuttgart',
+        'Dusseldorf',
+        'Leipzig',
+        'Dortmund',
+        'Essen'
+    ],
+    'United Kingdom': [
+        'London',
+        'Manchester',
+        'Birmingham',
+        'Leeds',
+        'Glasgow',
+        'Liverpool',
+        'Newcastle',
+        'Sheffield',
+        'Bristol',
+        'Edinburgh'
+    ]
+} 
