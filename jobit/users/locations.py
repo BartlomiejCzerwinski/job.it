240f5314fdd4a52976493a402dee0d19a1dc9ce8
BartlomiejCzerwinski <bartek2002@interia.pl>
2025-06-04 19:54:48 +0200

    Update locations.py

--- jobit/users/locations.py
+++ jobit/users/locations.py
@@ -34,5 +34,223 @@ LOCATIONS = {
         'Sheffield',
         'Bristol',
         'Edinburgh'
+    ],
+    'France': [
+        'Paris',
+        'Marseille',
+        'Lyon',
+        'Toulouse',
+        'Nice',
+        'Nantes',
+        'Strasbourg',
+        'Montpellier',
+        'Bordeaux',
+        'Lille'
+    ],
+    'Spain': [
+        'Madrid',
+        'Barcelona',
+        'Valencia',
+        'Seville',
+        'Zaragoza',
+        'Malaga',
+        'Murcia',
+        'Palma',
+        'Las Palmas',
+        'Bilbao'
+    ],
+    'Italy': [
+        'Rome',
+        'Milan',
+        'Naples',
+        'Turin',
+        'Palermo',
+        'Genoa',
+        'Bologna',
+        'Florence',
+        'Bari',
+        'Catania'
+    ],
+    'Netherlands': [
+        'Amsterdam',
+        'Rotterdam',
+        'The Hague',
+        'Utrecht',
+        'Eindhoven',
+        'Tilburg',
+        'Groningen',
+        'Almere',
+        'Breda',
+        'Nijmegen'
+    ],
+    'Sweden': [
+        'Stockholm',
+        'Gothenburg',
+        'Malmo',
+        'Uppsala',
+        'Vasteras',
+        'Orebro',
+        'Linkoping',
+        'Helsingborg',
+        'Jonkoping',
+        'Norrkoping'
+    ],
+    'Norway': [
+        'Oslo',
+        'Bergen',
+        'Stavanger',
+        'Trondheim',
+        'Drammen',
+        'Fredrikstad',
+        'Kristiansand',
+        'Sandnes',
+        'Tromso',
+        'Sarpsborg'
+    ],
+    'Switzerland': [
+        'Zurich',
+        'Geneva',
+        'Basel',
+        'Bern',
+        'Lausanne',
+        'Winterthur',
+        'Lucerne',
+        'St. Gallen',
+        'Lugano',
+        'Biel/Bienne'
+    ],
+    'Austria': [
+        'Vienna',
+        'Graz',
+        'Linz',
+        'Salzburg',
+        'Innsbruck',
+        'Klagenfurt',
+        'Villach',
+        'Wels',
+        'Sankt Polten',
+        'Dornbirn'
+    ],
+    'Czech Republic': [
+        'Prague',
+        'Brno',
+        'Ostrava',
+        'Plzen',
+        'Liberec',
+        'Olomouc',
+        'Usti nad Labem',
+        'Hradec Kralove',
+        'Pardubice',
+        'Zlin'
+    ],
+    'USA': [
+        'New York',
+        'Los Angeles',
+        'Chicago',
+        'Houston',
+        'Phoenix',
+        'Philadelphia',
+        'San Antonio',
+        'San Diego',
+        'Dallas',
+        'San Jose'
+    ],
+    'Ireland': [
+        'Dublin',
+        'Cork',
+        'Galway',
+        'Limerick',
+        'Waterford'
+    ],
+    'Finland': [
+        'Helsinki',
+        'Espoo',
+        'Tampere',
+        'Vantaa',
+        'Oulu'
+    ],
+    'Denmark': [
+        'Copenhagen',
+        'Aarhus',
+        'Odense',
+        'Aalborg',
+        'Esbjerg'
+    ],
+    'Portugal': [
+        'Lisbon',
+        'Porto',
+        'Braga',
+        'Coimbra',
+        'Aveiro'
+    ],
+    'Belgium': [
+        'Brussels',
+        'Antwerp',
+        'Ghent',
+        'Charleroi',
+        'Liege'
+    ],
+    'Romania': [
+        'Bucharest',
+        'Cluj-Napoca',
+        'Timisoara',
+        'Iasi',
+        'Brasov'
+    ],
+    'Hungary': [
+        'Budapest',
+        'Debrecen',
+        'Szeged',
+        'Miskolc',
+        'Pecs'
+    ],
+    'Estonia': [
+        'Tallinn',
+        'Tartu',
+        'Narva',
+        'Parnu',
+        'Viljandi'
+    ],
+    'Lithuania': [
+        'Vilnius',
+        'Kaunas',
+        'Klaipeda',
+        'Šiauliai',
+        'Panevezys'
+    ],
+    'Latvia': [
+        'Riga',
+        'Daugavpils',
+        'Liepaja',
+        'Jelgava',
+        'Jurmala'
+    ],
+    'Ukraine': [
+        'Kyiv',
+        'Lviv',
+        'Kharkiv',
+        'Dnipro',
+        'Odesa'
+    ],
+    'Bulgaria': [
+        'Sofia',
+        'Plovdiv',
+        'Varna',
+        'Burgas',
+        'Ruse'
+    ],
+    'Greece': [
+        'Athens',
+        'Thessaloniki',
+        'Patras',
+        'Heraklion',
+        'Larissa'
+    ],
+    'Turkey': [
+        'Istanbul',
+        'Ankara',
+        'Izmir',
+        'Bursa',
+        'Antalya'
     ]
 } 
