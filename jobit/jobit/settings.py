375e95d72fd3f79c39ca72f9845ec6b6f261e31b
BartlomiejCzerwinski <bartek2002@interia.pl>
2025-08-06 18:34:12 +0200

    add settings config for email

--- jobit/jobit/settings.py
+++ jobit/jobit/settings.py
@@ -110,9 +110,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'notifications.jobit@gmail.com'  # Replace with your Gmail
-EMAIL_HOST_PASSWORD = 'your-app-password'  # Replace with your app password
-DEFAULT_FROM_EMAIL = 'your-email@gmail.com'  # Replace with your Gmail
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = 'notifications.jobit@gmail.com'
+EMAIL_HOST_PASSWORD = 'luvr rnup adns qthm'
+DEFAULT_FROM_EMAIL = 'notifications.jobit@gmail.com'
+
 
 
 # Internationalization
