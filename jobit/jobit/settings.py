22e90f86cc83340f08bdf0a0707314ad934395f4
BartlomiejCzerwinski <bartek2002@interia.pl>
2025-08-06 08:05:10 +0200

    jobit notification email setup

--- jobit/jobit/settings.py
+++ jobit/jobit/settings.py
@@ -105,6 +105,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'notifications.jobit@gmail.com'  # Replace with your Gmail
+EMAIL_HOST_PASSWORD = 'your-app-password'  # Replace with your app password
+DEFAULT_FROM_EMAIL = 'your-email@gmail.com'  # Replace with your Gmail
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
