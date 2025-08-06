5537f1593968e33d1333320e1e41afeda2010d80
BartlomiejCzerwinski <bartek2002@interia.pl>
2025-08-06 18:43:00 +0200

    add password reset urls

--- jobit/users/urls.py
+++ jobit/users/urls.py
@@ -21,4 +21,9 @@ urlpatterns = [
     path('profile-photo/add/', views.add_profile_photo, name='add_profile_photo'),
     path('profile-photo/<int:user_id>/', views.get_profile_photo, name='get_profile_photo'),
     path('profiles/<int:user_id>/', views.public_profile, name='public_profile'),
+    path('password-reset/', views.password_reset_request, name='password_reset_request'),
+    path('password-reset/done/', views.password_reset_done, name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/', views.password_reset_confirm, name='password_reset_confirm'),
+    path('password-reset/complete/', views.password_reset_complete, name='password_reset_complete'),
+
 ]
